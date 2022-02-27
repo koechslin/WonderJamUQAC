5ebb5423c5c3b1269ab6f92b64230ae59a3b4d0d
koechslin <killian.oechslin@insa-lyon.fr>
2022-02-26 22:14:56 -0500

    Add PlayersSpaceships

---
+++ Assets/ScriptsQB/PlayersSpaceships.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayersSpaceships
+{
+    // Player 1
+    public static Sprite spriteP1;
+    public static int healthP1;
+    public static float fuelP1;
+    public static float speedP1;
+    public static float handlingP1;
+
+    // Player 2
+    public static Sprite spriteP2;
+    public static int healthP2;
+    public static float fuelP2;
+    public static float speedP2;
+    public static float handlingP2;
+}
