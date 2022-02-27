0ceb72eac7caf25062f2f27759b9b7b9aecfe154
alexis <alexis.shan-yan@insa-lyon.fr>
2022-02-27 11:55:31 -0500

    fixed asteroids

--- Assets/ScriptsQB/AsteroidsMovements.cs
+++ Assets/ScriptsQB/AsteroidsMovements.cs
@@ -101,9 +101,16 @@ public class AsteroidsMovements : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("P1") || collision.gameObject.CompareTag("P2"))
         {
-            Player player = collision.GetComponent<Player>();           
+            Player player = collision.GetComponent<Player>();     
+            GameObject shipTouched = collision.gameObject;
             if (!player.isInvincible)
             {
+                Vector3 playerPosition = shipTouched.transform.position;
+                Vector3 asteroidPosition = gameObject.transform.position;
+                Vector3 reboundForce = playerPosition - asteroidPosition;
+                shipTouched.GetComponent<Rigidbody2D>().AddRelativeForce(reboundForce * Time.deltaTime * 50, ForceMode2D.Impulse);
+
+                FindObjectOfType<AudioManager>().Play("Crash");
                 player.TakeDamage();
                 Destroy(gameObject);
             }
